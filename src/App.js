f6c5e40678d69e7a2959cb7908eb0d06938385c2
Suraj Birdawade <suraj.unify@circles.asia>
2025-05-29 11:36:08 +0530

    Initial commit of bug blaster project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+import TicketForm from './components/TicketForm';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Bug Blaster</h1>
+        <TicketForm />
+    
+        </div>
+    </div>
+  );
+}
+
+export default App;
